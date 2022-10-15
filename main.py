f073bd5996a7f67a3286eb5d8b6764531afa43b9
Vasiliy Shibayev <v_shibayev@mail.ru>
2022-10-15 10:07:39 +0600

    #004 Создание новой ветки

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-def fadd(a, b, c):
-    def _fadd(x):
-        return a * x * x + b * x + c
-    return _fadd
-
-# Комментарий обычный
-if __name__ == "__main__":
-    f = fadd(-2, 4, 1)
-    print(f(10))
+print("New Branch in Git")
